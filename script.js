ec093a7c38b2eec6d965980eccb48ccd9ffea9a8
kherma <kamil.herma01@gmail.com>
2020-11-26 22:16:37 +0100

    Add Leaflet library & geolocation

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pl/maps/@${latitude},${longitude},14z`);
+
+      const map = L.map('map').setView([latitude, longitude], 15);
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              closeButton: false,
+              className: 'cycling-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+      L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      }).addTo(map);
+    },
+    function () {
+      console.log('Could not get your location!');
+    }
+  );
