61901a74bfc5a13ad017ccb0c5e247d28eaf817a
kherma <kamil.herma01@gmail.com>
2020-11-27 14:47:42 +0100

    Code refactoring

--- script.js
+++ script.js
@@ -10,63 +10,80 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map, mapEvent;
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // Get coords
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      map = L.map('map').setView([latitude, longitude], 15);
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
-      // Handling clicks on map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-
-      L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-      }).addTo(map);
-    },
-    function () {
-      console.log('Could not get your location!');
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          console.log('Could not get your location!');
+        }
+      );
     }
-  );
+  }
+
+  _loadMap(position) {
+    // Get coords
+    const { latitude, longitude } = position.coords;
+    this.#map = L.map('map').setView([latitude, longitude], 15);
+
+    // Handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+
+    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    }).addTo(this.#map);
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    // Input field toggler
+    inputCadence.closest('div').classList.toggle('form__row--hidden');
+    inputElevation.closest('div').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(event) {
+    event.preventDefault();
+
+    // Clear input fields
+    const fields = form.getElementsByTagName('input');
+    Array.from(fields).forEach(field => {
+      field.value = '';
+      field.blur();
+    });
+
+    // Display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          closeButton: false,
+          className: `${inputType.value}-popup`,
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
 
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  // Clear input fields
-  const fields = form.getElementsByTagName('input');
-  Array.from(fields).forEach(field => {
-    field.value = '';
-    field.blur();
-  });
-
-  // Display marker
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        closeButton: false,
-        className: `${inputType.value}-popup`,
-      })
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
-
-// Input field toggler
-inputType.addEventListener('change', function (event) {
-  inputCadence.closest('div').classList.toggle('form__row--hidden');
-  inputElevation.closest('div').classList.toggle('form__row--hidden');
-});
+const app = new App();
